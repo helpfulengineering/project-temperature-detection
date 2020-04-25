ccc0fb5af500e20b34f4dbc971f98f8ac7439179
Helio Machado <0x2b3bfa0+git@googlemail.com>
2020-04-25 20:31:09 +0200

    Allow a button to trigger measurements in devices without sonar (#13)
    * Modularize trigger function
    
    * Complete M5StickC board specific parameters
    
    * Add traces
    
    * Change limit fever to 30 so as to be able to test with the skin temperature
    
    * Set stabilization time macro to 100ms
    
    Co-authored-by: Florian <1technophile@users.noreply.github.com>

--- firmware/temperature_detection/user_config.h
+++ firmware/temperature_detection/user_config.h
@@ -1,35 +1,51 @@
 #ifndef USER_CONFIG_H
 #define USER_CONFIG_H
 
-// Pins
-#define ULTRASOUND_TRIGGER_PIN  2
-#define ULTRASOUND_ECHO_PIN     3
-#define GREEN_INDICATOR_PIN     4
-#define ORANGE_INDICATOR_PIN    5
-#define RED_INDICATOR_PIN       6 // 13 to use Arduino Uno builtin LED
+// Switches
+// #define ENABLE_SONAR 
+// #define INVERT_INDICATORS
 
 // Times (bauds and milliseconds)
-#define SERIAL_MONITOR_SPEED    9600
-#define TIME_BETWEEN_READINGS   500
-#define SENSOR_STABILIZATION    3500
+#define SERIAL_MONITOR_SPEED        9600
+#define SENSOR_STABILIZATION_TIME   100
+#define TIME_BETWEEN_READINGS       500
+#define DISPLAY_TIME                2000
 
 // Sampling (count and milliseconds)
-#define TEMPERATURE_SAMPLES     3
-#define TEMPERATURE_INTERVAL    500
-#define DISTANCE_SAMPLES        5
-#define DISTANCE_INTERVAL       100 // > 60 as per Sparkfun HC-SR04 datasheet
+#define TEMPERATURE_SAMPLES         3
+#define TEMPERATURE_INTERVAL        500
+#define DISTANCE_SAMPLES            5
+#define DISTANCE_INTERVAL           100 // >60 as per Sparkfun HC-SR04 datasheet
 
 // Distances (centimeters)
-#define OFF_DISTANCE            30
-#define DETECTION_DISTANCE      15
+#define OFF_DISTANCE                30
+#define DETECTION_DISTANCE          15
 
 // Temperatures (celsius degrees)
-#define LIMIT_FEVER             20
+#define LIMIT_FEVER                 30
 
-// ESP32-specific
+// Pins
+#ifndef ULTRASOUND_TRIGGER_PIN
+#define ULTRASOUND_TRIGGER_PIN      2
+#endif
+#ifndef ULTRASOUND_ECHO_PIN
+#define ULTRASOUND_ECHO_PIN         3
+#endif
+#ifndef GREEN_INDICATOR_PIN
+#define GREEN_INDICATOR_PIN         4
+#endif
+#ifndef ORANGE_INDICATOR_PIN
+#define ORANGE_INDICATOR_PIN        5
+#endif
+#ifndef RED_INDICATOR_PIN
+#define RED_INDICATOR_PIN           6 // 13 to use Arduino Uno builtin LED
+#endif
+#ifndef BUTTON_PIN
+#define BUTTON_PIN                  7 // 37 to use BUTTON A on M5StickC
+#endif
 #ifdef ESP32
-#define ESP32_SDA_PIN           0
-#define ESP32_SCL_PIN           26
+#define ESP32_SDA_PIN               0
+#define ESP32_SCL_PIN               26
 #endif
 
 #endif
